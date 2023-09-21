447c23b28284dd645df5f7f7ed4812bb44113e09
Devanshu <devanshu@Devanshus-MacBook-Pro.local>
2023-09-21 17:14:31 +0530

    Magic happening + Created Services

--- src/components/Magic.jsx
+++ src/components/Magic.jsx
@@ -2,13 +2,40 @@ import { useState, useEffect } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
 
 const Magic = () => {
+  const [article, setArticle] = useState({ url: "", summary: "" });
+
+  const handleSubmit = async (e) => {
+    alert("Submit");
+  };
+
   return (
     <section className="mt-16 w-full max-w-xl">
       <div className="flex flex-col w-full gap-2">
         <form
           className="relative flex justify-center items-center"
-          onSubmit={() => {}}>
-          <input></input>
+          onSubmit={handleSubmit}>
+          <img
+            src={linkIcon}
+            alt="link_icon"
+            className="absolute left-0 my-2 ml-3 w-5"
+          />
+
+          {/* INPUT */}
+          <input
+            type="url"
+            placeholder="Enter a URL"
+            value={article.url}
+            onChange={(e) => {
+              setArticle({ ...article, url: e.target.value });
+            }}
+            required
+            className="url_input peer"
+          />
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700">
+            Go
+          </button>
         </form>
       </div>
     </section>
