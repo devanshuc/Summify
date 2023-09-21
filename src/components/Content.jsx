8d1743e4eff3887a01f7e45fb7da6c069bae5cc1
Devanshu <devanshu@Devanshus-MacBook-Pro.local>
2023-09-21 16:06:29 +0530

    Content section

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,7 +1,28 @@
 import { logo } from "../assets";
 
 const Content = () => {
-  return <div>Content</div>;
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="summify-logo" className="w-28 object-contain" />
+        <button
+          onClick={() => window.open("https://github.com/devanshuc/Summify")}
+          className="black_btn">
+          Github
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Summarize Articles with <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+
+      <h2 className="desc">
+        Instantly summarize articles with OpenAI's GPT model. Effortless,
+        efficient reading at your fingertips.
+      </h2>
+    </header>
+  );
 };
 
 export default Content;
--- src/components/Magic.jsx
+++ src/components/Magic.jsx
@@ -1,5 +1,18 @@
+import { useState, useEffect } from "react";
+import { copy, linkIcon, loader, tick } from "../assets";
+
 const Magic = () => {
-  return <div>Magic</div>;
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      <div className="flex flex-col w-full gap-2">
+        <form
+          className="relative flex justify-center items-center"
+          onSubmit={() => {}}>
+          <input></input>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Magic;
